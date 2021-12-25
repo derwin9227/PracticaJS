a289ed8ca37b6fafe4956b7c2cad5be8242485aa
derwin <derwin9227@gmail.com>
2021-12-25 09:21:35 -0500

    aplicando formulario a taller -precio y descuentos-

--- descuentos.js
+++ descuentos.js
@@ -1,7 +1,5 @@
-/* const precioOriginal = 100;
-const descuento = 15; */
 
-function calcularPrecioConDescuento(precio, descuento){
+function precioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
@@ -10,9 +8,25 @@ function calcularPrecioConDescuento(precio, descuento){
 }
 
 
-/* console.log({
-    precioOriginal,
-    descuento,
-    porcentajePrecioConDescuento,
-    precioConDescuento
-}); */
+function calcularPrecioConDescuento()
+    {
+        const inputPrecio = document.getElementById('inputPrecio');
+        const precio = parseFloat(inputPrecio.value);
+        const inputDescuento = document.getElementById('inputDescuento');
+        let descuento = parseFloat(inputDescuento.value);
+
+        if(precio > 0)
+        {
+            if(descuento)
+                {
+                    alert(`El precio total a pagar es: ${precioConDescuento(precio, descuento)}`);
+                }else
+                {
+                    descuento = 0;
+                    alert(`El precio total a pagar es: ${precioConDescuento(precio, descuento)}`);
+
+                }
+        }else{
+            alert('Por favor ingrese un precio para calcular');
+        }
+}
